1a261e69afd6bd5780ced729d5631050093aef6f
liminalAngel <veronikabogdanova268@gmail.com>
2024-02-14 13:56:44 +0400

    tried to make it work

--- wrappers/Root.ts
+++ wrappers/Root.ts
@@ -1,13 +1,31 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Dictionary, Sender, SendMode, toNano } from '@ton/core';
+import { DaoDictType } from './utils/daoDictValue';
+import { crc32 } from './utils/crc32';
 
-export type RootConfig = {};
+export type RootConfig = {
+    maxDaoUsers: number | bigint;
+    minVotesToComplete: number | bigint;
+    daoDict: Dictionary<bigint, DaoDictType>;
+};
 
 export function rootConfigToCell(config: RootConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeUint(1, 64)
+        .storeRef(
+            beginCell()
+                .storeUint(config.maxDaoUsers, 32)
+                .storeUint(config.minVotesToComplete, 32)
+            .endCell()
+        )
+        .storeDict(config.daoDict)
+        .storeDict(Dictionary.empty())
+    .endCell();
 }
 
 export class Root implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static readonly ADD_MEMBER = crc32('add_member');
 
     static createFromAddress(address: Address) {
         return new Root(address);
@@ -22,6 +40,21 @@ export class Root implements Contract {
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
         await provider.internal(via, {
             value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendAddMember(
+        provider: ContractProvider, 
+        via: Sender,
+        opts: {
+            daoMemberId: bigint;
+            
+        }
+    ) {
+        await provider.internal(via, {
+            value: toNano('0.05'),
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
