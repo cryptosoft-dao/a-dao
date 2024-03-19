e7818ff83acfcae4e09a8bb63919ccc55bb07bb4
Some Wallet <somewallet@gmail.com>
2024-03-20 00:20:10 +0300

    successful scaffold

---
+++ dapp/src/wrappers/Config.ts
@@ -0,0 +1,38 @@
+// A DAO Master
+
+export const ADaoMasterOperationCodes = {
+    SendDeployMessage: 0,
+    WithdrawFunds: 1,
+    ChangeADaoMasterOwner: 2,
+}
+
+// A DAO
+
+export const ADaoOperationCodes = {
+    ProcessDeployMessage: 0,
+    MasterLog: 1,
+    ActivateADao: 2,
+    ProposeTransaction: 3,
+    ApproveTransaction: 4,
+    GetProfit: 5,
+    AcceptInvitationToADao: 6,
+    BuyPoints: 7,
+    InviteToADao: 8,
+    ChangeMyAddress: 9,
+    QuitADao: 10,
+};
+
+export const ADaoTransactionTypes = {
+    TransactionWithoutType: 0,
+    InviteAddress: 1,
+    DeleteAddress: 2,
+    WithdrawProfit: 3,
+    DistributeTon: 4,
+    ArbitraryTransaction: 5,
+    UpdateAgreementPercent: 6,
+    TransferPoints: 7,
+    PutUpPointsForSale: 8,
+    DeletePendingInvitations: 9,
+    DeletePendingTransactions: 10,
+}
+
